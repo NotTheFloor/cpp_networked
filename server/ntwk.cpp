fb52c9a2df17bdb8f345d541051b17a50802a549
NotTheFloor <charlesgthomas644@gmail.com>
2024-09-01 15:21:59 -0400

    Minor changed to ntwk.cpp

--- server/ntwk.cpp
+++ server/ntwk.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cstdint>
 #include <functional>
 #include <iostream>
 #include <sys/socket.h>
@@ -18,6 +19,48 @@
 #define LISTEN_BACKLOG 5
 #define MAX_EVENTS 10
 #define MAX_CONN_ATTEMPTS 3
+
+std::unique_ptr<BasePacket> packetFactory(uint16_t type)
+{
+    switch (type) {
+        case 1: // Need to define message types somewhere still
+            return std::make_unqiue<ConnReqPacket>();
+        case 2:
+            return std::make_unique<MessagePacket>();
+        default:
+            return nullptr;
+    }
+}
+
+void sendPacket(int sock, uint16_t type, const BasePacket &basePacket)
+{
+    std::vector<uint8_t> payload = basePacket.serialize();
+    uint16_t payload_length = static_cast<uint16_t>(payload.size());
+
+    TcpPcktHeader header = { htons(type), htons(payload_length) };
+
+    send(sock, &header, sizeof(header), 0);
+    send(sock, payload.data(), payload_length, 0);
+}
+
+std::unique_ptr<BasePacket> recvPacket(int sock, std::function<std::unique_ptr<BasePacket> (uint16_t)> packetFactory)
+{
+    TcpPcktHeader header;
+    recv(sock, &header, sizeof(header), 0);
+
+    uint16_t type = ntohs(header.type);
+    uint16_t length = ntohs(header.length);
+
+    std::vector<uint8_t> payload(length);
+    recv(sock, paylaod.data(), length, 0);
+
+    std::unique_ptr<BasePacket> basePacket = packetFactory(type);
+    if (basePacket) {
+        basePacket->deserialize(payload);
+    }
+
+    return basePacket;
+}
 
 void pushEvent(SharedResources &sharedResources, std::unique_ptr<BaseEvent>(event))
 {
